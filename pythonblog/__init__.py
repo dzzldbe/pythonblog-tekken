3266cfc31256e60c4f68dbcebbb4f86bafb2dafa
Askar Mukhanov <mukhanov1@gmail.com>
2025-05-30 18:12:13 +0700

    Stable version before making changes to config

--- pythonblog/__init__.py
+++ pythonblog/__init__.py
@@ -1,14 +1,20 @@
+import os
+
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
 
-from pythonblog.config import Config
-
 app = Flask(__name__)
-app.config.from_object(Config)
-# app.config.from_pyfile
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_APP")
+app.config["MAIL_USERNAME"] = os.environ.get("MY_GMAIL")
+
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt()
