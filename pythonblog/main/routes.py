5474a4be8879817bfdb83dcb2698ac8f2771043f
Askar Mukhanov <mukhanov1@gmail.com>
2025-06-04 19:37:14 +0700

    new pattern for background, preparing layout template new_try.css been heavily modified

--- pythonblog/main/routes.py
+++ pythonblog/main/routes.py
@@ -9,7 +9,7 @@ main = Blueprint("main", __name__)
 @main.route("/home")
 def home():
     page = request.args.get("page", default=1, type=int)
-    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=7)
 
     # sorted_posts= sorted(posts.items)
     return render_template("home.html", posts=posts)
