d9643834365adb9077b7b3fb44a16d4b0f118d6b
Askar Mukhanov <mukhanov1@gmail.com>
2025-06-04 12:15:54 +0700

    Working on new css and layout

--- pythonblog/main/routes.py
+++ pythonblog/main/routes.py
@@ -18,3 +18,10 @@ def home():
 @main.route("/about")
 def about():
     return render_template("about.html", title="About")
+
+
+@main.route("/home2")
+def home_2():
+    page = request.args.get("page", default=1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
+    return render_template("home_2.html", title="Home", posts=posts)
